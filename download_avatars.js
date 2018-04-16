26ed39d397bf289cb876d775c744b6ea29c36367
spierre95 <stephen.pierre20@gmail.com>
2018-04-16 18:24:54 +0000

    added authorization

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,22 @@
 var request = require('request');
+var authorize = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+
+  headers: {
+      'User-Agent': 'request'
+      'Authorization': authorize
+    }
+
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
