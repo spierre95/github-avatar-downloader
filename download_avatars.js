06279bf1c8e6b774a43d136f6ed3503fb32ea42d
spierre95 <stephen.pierre20@gmail.com>
2018-04-16 21:03:28 +0000

    parsed JSON, iterates over avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -17,12 +17,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
 
 request(options, function(err, res, body) {
-  cb(err, body);
+  cb(err, JSON.parse(body));
 });
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
+  for (var element of result){
+    console.log(element.avatar_url)
+  }
 });
 
